6a68fe3cb1a3d3c34dda60e4a05023ba719c9819
JordiFFS <117864372+JordiFFS@users.noreply.github.com>
2025-12-16 19:14:45 -0500

    [UPDATE] Settings

--- backend/DJBase/settings.py
+++ backend/DJBase/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'DJBase.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': '',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '*',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
